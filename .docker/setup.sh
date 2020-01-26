e72cbed5520947321c6a6db35c02a2b2f6e8ffe3
Mateus Soares <mateussoaress1997@gmail.com>
2020-01-26 10:58:42 -0300

    Database to docker-compose

--- .docker/setup.sh
+++ .docker/setup.sh
@@ -6,4 +6,5 @@ chmod 600 private.key public.key
 chown www-data:www-data private.key public.key
 echo -e "OAUTH2_ENCRYPTION_KEY='$(php -r 'echo base64_encode(random_bytes(32)), PHP_EOL;')'" >> .env
 composer install
+php bin/console doctrine:migrations:migrate --no-interaction
 apache2-foreground
